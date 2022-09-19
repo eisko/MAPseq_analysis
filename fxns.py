77f02f6ea14a81221986429dbee041dc889cddf5
eisko <buttaafly@gmail.com>
2022-09-19 17:39:26 -0400

    added fxns.py file

---
+++ fxns.py
@@ -0,0 +1,66 @@
+# script with fxns
+
+import pandas as pd
+import numpy as np
+
+
+
+def sort_by_celltype(proj):
+    """
+    Function takes in projection matrix and outputs matrix sorted by the 3 major celltypes:
+    - IT = intratelencephalic (projects to cortical and/or Striatum), type = 10
+    - CT = corticalthalamic (projects to thalamus w/o projection to brainstem), type = 100
+    - PT = pyramidal tract (projects to brainstem += other areas), type = 1000
+    Returns 3 dataframes containing cells for the 3 cell types
+    """
+    # neurons to projections to any of the following areas are considered PT cells
+    pt_areas = ["SNr","SCm","PG","PAG","RN"]
+ 
+    ds = proj
+
+    # 1. isolate PT cells
+    pt_counts = ds[pt_areas].sum(axis=1)
+    pt_idx = ds[pt_counts>0].index
+    ds_pt = ds.loc[pt_idx,:]
+    ds_pt = ds_pt.sort_values('PAG', ascending=False)
+    ds_pt['type'] = 1000
+
+    # Isolate remaining non-PT cells
+    ds_npt = ds.drop(pt_idx)
+
+    # Identify CT cells by thalamus projection
+    th_idx = ds_npt['TH'] > 0
+    ds_th = ds_npt[th_idx]
+    ds_th = ds_th.sort_values('TH', ascending=False)
+    ds_th['type'] = 100
+
+    # Identify IT cells by the remaining cells (non-PT, non-CT)
+    ds_nth = ds_npt[~th_idx]
+    ds_nth = ds_nth.sort_values(['OMCc','AUD','STR'],ascending=False)
+    ds_nth['type'] = 10
+
+    # combine IT and CT cells
+    ds_npt = pd.concat([ds_nth, ds_th])
+
+    # combine IT/CT and PT cells
+    sorted = pd.concat([ds_npt,ds_pt],ignore_index=True)
+
+    sorted=sorted.reset_index(drop=True)
+    
+    return sorted
+
+def est_proj_prob(bin_proj, reps=1000, sample_size=300):
+    """
+    funtion takes subsample of projection matrix and repetitions as input.
+    Funtion outputs vector where dim0 = projection probability of subsample, and dim1 = repetition
+    """
+    
+    est_probs = []
+    for i in range(reps):
+        new = bin_proj.sample(sample_size)
+        est_probs.append(new.sum()/sample_size)
+        
+#         if i%100 == 0:
+#             print('finished simulation', i)
+        
+    return np.array(est_probs)
